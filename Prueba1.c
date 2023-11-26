56d5bbe6c7e09cccedef3f7fe659d00d3b9e73ca
RobertoLoor <151573507+RobertoLoor@users.noreply.github.com>
2023-11-26 18:29:08 +0100

    Update Prueba1.c

--- Prueba1.c
+++ Prueba1.c
@@ -6,19 +6,36 @@
 #include <time.h>
 
 
+//DEFINICIONES CONSTANTES
+#define NOMBRE 50
+#define PIN 4
+#define IBAN 25
+
+
+//MODULOS
+void usuarioSoN();
+void ususi();
+void ingr();
+void retiro();
+void crearUsuario(struct Usuario *usuario);
+void generarIban(char *iban);
+void generarPin(char *pin);
+
+
 
 //ESTRUCTURA DE USUARIOS
 struct Usuario {
-  char nombre[50];
-  char contrasena[20];
+  char nombre[NOMBRE];
+  char pin[PIN];
   float saldo;
-  char iban[30];
-};
+  char iban[IBAN];
+} usuario;
 
 
 
 //MAIN
 int main() {
+  struct Usuario usuario;
   
   return 0;
 }
@@ -26,30 +43,28 @@ int main() {
 
 
 //MODULOS
-
 //MODULO QUE PREGUNTA AL USUARIO SI TIENE UNA CUENTA O NO
-void usuarioSoN(){
-  int usu;
-  
+void usuarioSoN(*ususn){
+  int ususn;
   printf("Bienvenido al menu\n");
   printf("1. Tengo una cuenta\n"); 
   printf("2. Crear Usuario\n"); 
   scanf("%d", &usu);
-  
+  *ususn = ususn;
 }
 
 
-
+/////MODULOS DE USUARIO SI
 //EN CASO DE QUE TENGA CUENTA, SE LE MUESTRA ESTE MENU AL USUARIO
-void ususi(){
+void ususi(*usu){
   int usu;
-  
   printf("Seleccione su operacion\n");
   printf("1. Ingresas dinero\n"); 
   printf("2. Retirar dinero\n"); 
   printf("3. Estado de cuenta\n"); 
+  printf("4. Transferencia dinero\n");
   scanf("%d", &usu);
-  
+  *usu = usu;
 }
 
 
@@ -57,30 +72,25 @@ void ususi(){
 //MODULO PARA INGRESOS DE DINERO
 void ingr(){
   int din, conf;
-  
-  printf("Introduzca la cantidad de dinero que desea ingresar");
+  printf("Introduzca la cantidad de dinero que desea ingresar\n");
   scanf("%d", &din);
-
-  if( din % 10 == 0){
-    printf("Confirme su operacion s/n");
-    scanf("%c", &conf);
-
-      if(conf == s){
-        printf("Se ha ingresado %d en su cuenta", din);
-        printf("Gracias por utilizar nuestro servicio");       
-      }
-      else{
-        printf("Operacion cancelada");
-        printf("Gracias por utilizar nuestro servicio");       
-      }
-          
-  }
-
-  else {
-    printf("La cantidad introducida debe ser un multiplo de 10");
-    printf("Operacion fallida");
-    printf("Gracias por utilizar nuestro servicio");
-  }
+    if( din % 10 == 0){
+      printf("Confirme su operacion s/n\n");
+      scanf("%c", &conf);
+        if(conf == 's'){
+          printf("Se ha ingresado %d en su cuenta\n", din);
+          printf("Gracias por utilizar nuestro servicio\n");       
+        }
+        else{
+          printf("Operacion cancelada\n");
+          printf("Gracias por utilizar nuestro servicio\n");       
+        }       
+    }
+    else {
+      printf("La cantidad introducida debe ser un multiplo de 10\n");
+      printf("Operacion fallida\n");
+      printf("Gracias por utilizar nuestro servicio\n");
+    }
 }
 
 
@@ -88,42 +98,59 @@ void ingr(){
 //MODULO PARA RETIRO DE DINERO
 void retiro(){
   int dinre, confre;
-  
-  printf("Introduzca la cantidad de dinero que desea retirar");
+  printf("Introduzca la cantidad de dinero que desea retirar\n");
   scanf("%d", &dinre);
-
-  if( dinre % 10 == 0){
-    printf("Confirme su operacion s/n");
-    scanf("%c", &confre);
-
-      if(confre == s){
-        printf("Se ha retirado %d de su cuenta", dinre);
-        printf("Gracias por utilizar nuestro servicio");       
-      }
-      else{
-        printf("Operacion cancelada");
-        printf("Gracias por utilizar nuestro servicio");       
-      }
-          
-  }
-
-  else {
-    printf("La cantidad introducida debe ser un multiplo de 10");
-    printf("Operacion fallida");
-    printf("Gracias por utilizar nuestro servicio");
-  }
+    if( dinre % 10 == 0){
+      printf("Confirme su operacion s/n\n");
+      scanf("%c", &confre);
+        if(confre == 's'){
+          printf("Se ha retirado %d de su cuenta\n", dinre);
+          printf("Gracias por utilizar nuestro servicio\n");       
+        }
+        else{
+          printf("Operacion cancelada\n");
+          printf("Gracias por utilizar nuestro servicio\n");       
+        }
+    }
+    else {
+      printf("La cantidad introducida debe ser un multiplo de 10\n");
+      printf("Operacion fallida\n");
+      printf("Gracias por utilizar nuestro servicio\n");
+    }
 }
 
 
 
+
+
+
+
+
+/////MODULOS USUARIO NO
+void crearUsuario(struct Usuario *usuario){
+  
+  
+}
+
+
 // MODULO PARA GENERAR EL IBAN
 void generarIban(char *iban) {
-
-    strcpy(iban, "ES");
-
-    for (int i = 2; i < 18; ++i) {
+  int i;
+  srand( time ( NULL ) );
+  strcpy(iban, "ES00");
+    for (i = 4; i < 24; ++i) {
         iban[i] = '0' + rand() % 10;
+    }  
+  iban[24] = '\0';
+}
+
+
+
+void generarPin(char *pin) {
+  int i;
+  srand( time( NULL ) );
+    for (i = 0; i < 4; i++) {
+      pin[i] = '0' + rand() % 10;
     }
-    
-    iban[18] = '\0';
+      pin[4] = '\0';
 }
